eecc4ea2e63253bc985489a7bf1f8341a597fd69
Sultan Abdualiyev <190103196@stu.sdu.edu.kz>
2021-02-25 23:02:42 +0600

    create clients table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/home', function () {
     return view('home');    
 })->name('home');
